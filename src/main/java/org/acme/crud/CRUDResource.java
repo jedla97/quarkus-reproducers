a9953156e6e01b5789edc5948d10eb731291f497
Jakub Jedlicka <jjedlick@redhat.com>
2023-12-06 10:05:27 +0100

    Add reproducer for permission class inside the resource class

--- src/main/java/org/acme/crud/CRUDResource.java
+++ src/main/java/org/acme/crud/CRUDResource.java
@@ -1,4 +1,64 @@
 package org.acme.crud;
 
+import io.quarkus.arc.Arc;
+import io.vertx.ext.web.RoutingContext;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+
+import io.quarkus.security.PermissionsAllowed;
+
+import java.security.BasicPermission;
+import java.security.Permission;
+import java.util.Collection;
+import java.util.Collections;
+
+@Path("/crud")
 public class CRUDResource {
+
+    @PermissionsAllowed("create")
+    @PermissionsAllowed("update")
+    @POST
+    @Path("/modify/repeated")
+    public String createOrUpdate() {
+        return "modified";
+    }
+
+    @PermissionsAllowed(value = {"create", "update"}, inclusive=true)
+    @POST
+    @Path("/modify/inclusive")
+    public String createOrUpdate(Long id) {
+        return id + " modified";
+    }
+
+    @PermissionsAllowed({"see:detail", "see:all", "read"})
+    @GET
+    @Path("/id/{id}")
+    public String getItem(String id) {
+        return "item-detail-" + id;
+    }
+
+    @PermissionsAllowed(value = "list", permission = CustomPermission.class)
+    @Path("/list")
+    @GET
+    public Collection<String> list(@QueryParam("query-options") String queryOptions) {
+        // your business logic comes here
+        return Collections.emptySet();
+    }
+
+    public static class CustomPermission extends BasicPermission {
+
+        public CustomPermission(String name) {
+            super(name);
+        }
+
+        @Override
+        public boolean implies(Permission permission) {
+            var event = Arc.container().instance(RoutingContext.class).get();
+            var publicContent = "public-content".equals(event.request().params().get("query-options"));
+            var hasPermission = getName().equals(permission.getName());
+            return hasPermission && publicContent;
+        }
+    }
 }
