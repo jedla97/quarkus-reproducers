6e6a28f1f549778c6b69cf1fe0d914dd539d8849
Jakub Jedlicka <jjedlick@redhat.com>
2025-03-11 19:01:05 +0100

    Add management

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -1,13 +1,19 @@
 package org.acme;
 
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+
+import java.net.URL;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class GreetingResourceTest {
+
+    @TestHTTPResource(value="/health/live", management=true)
+    URL url;
 
     @Test
     public void testHelloEndpoint() {
@@ -16,6 +22,14 @@ public class GreetingResourceTest {
           .then()
              .statusCode(200)
              .body(is("Hello from RESTEasy Reactive"));
+    }
+
+    @Test
+    public void testManagementEndpoint() {
+        given()
+                .when().get(url)
+                .then()
+                .statusCode(200);
     }
 
 }
