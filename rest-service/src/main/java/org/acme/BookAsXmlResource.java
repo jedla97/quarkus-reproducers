490758e5c317e1d6e009e86467f6b10f4754c1da
Jakub Jedlicka <jjedlick@redhat.com>
2025-05-27 11:01:33 +0200

    Multimodule Reproducer

--- rest-service/src/main/java/org/acme/BookAsXmlResource.java
+++ rest-service/src/main/java/org/acme/BookAsXmlResource.java
@@ -5,12 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+@Produces(MediaType.APPLICATION_XML)
+@Path("/book/xml")
+public class BookAsXmlResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "<book><title>Title in Xml</title></book>";
     }
 }
