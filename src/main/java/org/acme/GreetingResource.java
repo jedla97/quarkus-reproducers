772c940858b335473c6e1e24b5f1432d2d0c414b
Jakub Jedlicka <jjedlick@redhat.com>
2025-10-02 21:04:53 +0200

    Add jfr-eproducer

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,13 +4,42 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jdk.jfr.Recording;
+import jdk.jfr.consumer.RecordedEvent;
+import jdk.jfr.consumer.RecordingFile;
 
-@Path("/hello")
+import java.io.IOException;
+import java.util.List;
+
+@Path("")
 public class GreetingResource {
 
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
+
+    @GET
+    @Path("dump")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String dump() {
+        final java.nio.file.Path dumpPath = java.nio.file.Path.of("./dump.jfr");
+        try {
+            try (Recording r = new Recording()) {
+                r.start();
+                r.stop();
+                r.dump(dumpPath);
+            } catch (Exception e) {
+                throw new RuntimeException("JEDLA");
+            }
+            List<RecordedEvent> recordedEvents = RecordingFile.readAllEvents(dumpPath);
+            return recordedEvents.toString();
+        } catch (RuntimeException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
